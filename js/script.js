d167963a9471f707838931dd8ada68ae6b58551f
Andrew M. Perez <AxMPerez@gmail.com>
2018-02-19 16:47:55 -0500

    Inlcude greeting message

--- js/script.js
+++ js/script.js
@@ -18,6 +18,7 @@ function loadData() {
     var cityStr = $('#city').val();
     var address = streetStr + ', ' + cityStr;
 
+    $greeting.text('So, you want to live at ' + address + '?');
 
     var streetviewUrl = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
     $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
